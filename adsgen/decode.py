abea7a01c34bfc74fec36876ca88fae30d9b8569
Hilary B. Brenum (MBP) <hilary@hilarybisenieks.com>
2018-09-19 11:25:59 -0700

    You guessed it: delinting

--- adsgen/decode.py
+++ adsgen/decode.py
@@ -1,11 +1,14 @@
 from unidecode import unidecode
 from re import sub
 
+
 def decode(str):
-    return unidecode(unicode(str,'utf_8'))
+    return unidecode(unicode(str, 'utf_8'))
+
 
 def sanitize(str):
-    return sub('[ \'-]','',str)
+    return sub('[ \'-]', '', str)
+
 
 def sane(str):
     return sanitize(decode(str))
