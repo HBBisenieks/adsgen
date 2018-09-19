719429c76f9fd7c7293943a53950bf980f63933f
Hilary B. Brenum (MBP) <hilary@hilarybisenieks.com>
2018-09-19 14:28:04 -0700

    Version update to accommodate bug fix + efficiency changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='adsgen',
-    version='0.2.1',
+    version='0.2.2',
     description="""Generate usernames and other account info from Blackbaud
                 exports""",
     url="https://headroyce.org",
